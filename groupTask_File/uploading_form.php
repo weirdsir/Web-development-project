370968ff59c570549f7c41a7a5bfde572124ed20
youneskalami <92359157+youneskalami@users.noreply.github.com>
2022-02-14 10:12:13 +0200

    groupTask_File

---
+++ groupTask_File/uploading_form.php
@@ -0,0 +1,7 @@
+<?php include "db.php";?>
+<hr><h2>Upload your files </h2>
+
+<form enctype="multipart/form-data" action="upload.php" method="POST">
+    Select file to upload: <input name="userfile" type="file" required><br>
+    <input type="submit" value="Upload File" />
+</form>
