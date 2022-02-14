fa218bb95d894fb658f1525859b495f2f6699d87
youneskalami <92359157+youneskalami@users.noreply.github.com>
2022-02-14 11:13:29 +0200

    Update upload.php

--- groupTask_File/upload.php
+++ groupTask_File/upload.php
@@ -4,9 +4,6 @@ date_default_timezone_set('Europe/Helsinki');
 
 $dest = "uploaded_files/".basename($_FILES["userfile"] ["name"]);
 
-
-ini_set(upload_max_filesize, "4000M");
-
     if (move_uploaded_file($_FILES["userfile"] ["tmp_name"], $dest)){
         echo "The file has been successfully uploaded. <br>";
         echo "<a href='uploaded_files/'> Click to see list of files </a>";
