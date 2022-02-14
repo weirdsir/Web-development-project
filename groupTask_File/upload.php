f770740cb9daa3596ed1e915e41372fa1a21ae5d
youneskalami <youneskalami@gmail.com>
2022-02-14 10:21:27 +0200

    max size

--- groupTask_File/upload.php
+++ groupTask_File/upload.php
@@ -3,9 +3,14 @@ include "db.php";
 date_default_timezone_set('Europe/Helsinki');
 
 $dest = "uploaded_files/".basename($_FILES["userfile"] ["name"]);
+
+
+if (ini_set(upload_max_filesize, "4000M")) {
+
     if (move_uploaded_file($_FILES["userfile"] ["tmp_name"], $dest)){
         echo "The file has been successfully uploaded. <br>";
         echo "<a href='uploaded_files/'> Click to see list of files </a>";
     }
+}
     else "Unable to upload file";
 ?>
