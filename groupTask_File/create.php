85769a26f29e39511738a53a59070d3a607b3b62
slavakich7 <slavakich7@gmail.com>
2022-02-15 14:10:24 +0200

    group task

--- groupTask_File/create.php
+++ groupTask_File/create.php
@@ -1,13 +1,13 @@
 <?php
 # 1. Create/read a text file by using approprite php functions 
-echo "<hr><h2>Creating a file </h2>";
-$newfile = fopen('file.txt','w') or die ("Failed to create a file");
+
     # Step 1: check if file exists or not
 echo "<hr><h2>Checking file </h2>"; 
 if (file_exists("file.txt")) echo "File exists";
 else echo "File does not exist";
     # Step 2: Open the file using appropriate mode. (each member opens the file in different mode)
-
+echo "<hr><h2>Creating a file </h2>";
+$newfile = fopen('file.txt','w') or die ("Failed to create a file");
     # Step 3: Use fwrite/fread function to write/read on the file your team name and members name. 
 echo "<hr><h2>fwrite/fread function</h2>"; 
 $txt = "Team 10: Vjatšeslav Semjonov, Arina Pokatinska, Younes Kalami <br>";
