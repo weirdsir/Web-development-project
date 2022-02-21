1b7de8f18f5e76fc66807e3a0e7b218e6eb361e8
youneskalami <youneskalami@gmail.com>
2022-02-21 13:24:19 +0200

    email validation

--- TEAM10/form.php
+++ TEAM10/form.php
@@ -5,7 +5,7 @@
 <div id="users" style="text-align: center; margin: auto; width: 500px; padding: 150px; ">
  <form action="register.php" method="post">
      <input type="text" name="username" placeholder="Username..."> <br>
-     <input type="text" name="email" placeholder="Email..."><br>
+     <input type="email" name="email" placeholder="Email..."><br>
      <input type="password" name="password" placeholder="Password..."><br>
      <input type="password" name="password_conf" placeholder="Repeat password..."><br>
      <input type="submit" name="do-register" value="Register"><br>
