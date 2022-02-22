6be8dcdbae66ad3d327029592ecfc8bed1cda927
slavakich7 <slavakich7@gmail.com>
2022-02-22 12:16:57 +0200

    Contact form

--- TEAM10/contact.php
+++ TEAM10/contact.php
@@ -6,14 +6,14 @@
   } catch (exception $exc) {
     echo $exc->getTraceAsString();
   }
-  if (isset($_POST['firstname']) &&  isset($_POST['lastname']) &&  isset($_POST['email']) &&  isset($_POST['message'])) {
+  if (isset($_POST['name']) &&  isset($_POST['mail']) &&  isset($_POST['subject']) &&  isset($_POST['message'])) {
 
   
-  $firstname = $_POST['firstname'];
-  $lastname = $_POST['lastname'];
-  $email = $_POST['email'];
+  $name = $_POST['name'];
+  $mail = $_POST['mail'];
+  $subject = $_POST['subject'];
   $message = $_POST['message'];
-  $is_insert = $conn->query("INSERT INTO `contactform`(`First name`, `Last name`, `Email`, `Message`) VALUES ('$firstname','$lastname','$email','$message')");
+  $is_insert = $conn->query("INSERT INTO `contactform`(`Name`, `Email`, `subject`, `Message`) VALUES ('$name','$mail','$subject','$message')");
   if($is_insert == TRUE) {
     echo "<h2>Thanks, your message sent</h2>";
     exit();
@@ -25,15 +25,17 @@
     <div class="container-fluid" style="background-color: #FFF0E3;">
         <div class="p2">Tel: +358 446584125<br>
         Email: info@recipe.com<br><br></div>
-        <form method="post"  >
-          <input type="text1" name="firstname" placeholder="First name"><br><br>
-          <input type="text1" name="lastname" placeholder="Last name"><br><br>
-          <input type="email" name="email" placeholder="Email"><br><br>
+        <form method="post" action="contactform.php" >
+          <input type="text1" name="name" placeholder="Full name"><br><br>
+          <input type="text1" name="mail" placeholder="Email"><br><br>
+          <input type="text1" name="subject" placeholder="Subject"><br><br>
           <textarea class="message" name="message" placeholder="Message"></textarea><br>
-          <button type = 'submit1' name = 'submit'>Comment</button><br><br>
+          <button type = "submit1" name = "submit">Comment</button><br><br>
         </form>
     </div>
 
     <?php
 include 'footer.php';
 ?>
+
+    
