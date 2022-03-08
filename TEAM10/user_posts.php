e263662c34883d0142fca1bfc3e9639a79426840
youneskalami <youneskalami@gmail.com>
2022-03-08 11:52:26 +0200

    combined sql files

--- TEAM10/user_posts.php
+++ TEAM10/user_posts.php
@@ -3,13 +3,14 @@ session_start();
 include 'header.php';
 require 'db.php';
 
-// $query="SELECT * FROM posts";
-// $results = mysql_query($query);
-// $res = mysql_result($results, 0);
+$query="SELECT * FROM posts";
+$results = mysql_query($query);
+$res = mysql_result($results, 1);
 
-// while ($res->fetchInto($row)) {
-//     echo "<form id=\"$row[0]\" name=\"$row[0]\" method=post action=\"\"><td style=\"border-bottom:1px solid black\">$row[0]</td><td style=\"border-bottom:1px solid black\"><input type=hidden name=\"remove\" value=\"$row[0]\"><input type=submit value=Remove></td><tr></form>\n";
-// }
+while ($res->fetchInto($row)) {
+     echo "<form id='$row[1]' name='$row[1]' method=post action=''><td style='border-bottom:1px solid black'>$row[1]</td>
+     <td style='border-bottom:1px solid black'><input type=hidden name='remove' value='$row[1]'><input type=submit value=Remove></td><tr></form>";
+ }
 ?>
 
 <?php include 'footer.php' ?>
